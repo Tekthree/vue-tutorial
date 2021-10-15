4173c973b1e30e705f35e825bcb685cd154b6a8c
Tek Jones <tekthree@gmail.com>
2021-10-14 17:26:34 -0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
 import { createApp } from 'vue'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
